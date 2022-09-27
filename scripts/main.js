aae022d2165be581a1d63aa9b67946b122515c61
Tayebe <103579405+zukunft-2022@users.noreply.github.com>
2022-09-27 21:09:20 +0200

    gym final project

---
+++ scripts/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+  $('.nav-button').click(function(){
+     $('.nav-button').toggleClass('change')
+   })
+})
+$(window).scroll(function(){
+    let position = $(this).scrollTop();
+    if(position >= 200){
+      $('.nav-menu').addClass('custom-navbar ')
+    }else{
+        $('.nav-menu').removeClass('custom-navbar ')
+    }
+
+})
+
+$(window).scroll(function(){
+    let position = $(this).scrollTop();
+    if(position >= 700){
+      $('.award-text').addClass('fromRight')
+      $('.award-img').addClass('fromLeft')
+    }else{
+        $('.award-text').removeClass('fromRight')
+      $('.award-img').removeClass('fromLeft')
+    }
+
+})
+$('.gallery-list-item').click(function(){
+    let value = $(this).attr('data-filter');
+    $(this).addClass('active-item').siblings().removeClass('active-item')
+
+    if(value=== 'all'){
+       $('.filter').show(300)
+    }else{
+       $('.filter').not('.' + value).hide(300);
+       $('.filter').filter('.' + value).show(300);
+    }
+})
+
+$(window).scroll(function(){
+    let position = $(this).scrollTop();
+    console.log(position)
+    if(position >= 4000){
+        $('.card-3').addClass('moveFromLeft')
+      $('.card-1').addClass('moveFromRight')
+      $('.card-2').addClass('moveFromBotton')
+      
+    }else{
+        $('.card-3').addClass('moveFromLeft')
+        $('.card-1').removeClass('moveFromRight')
+      $('.card-2').removeClass('moveFromBotton')
+      
+    }
+
+})
